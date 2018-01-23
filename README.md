5e42d0f13499f35fd333683a92c33c85c5f3a63c
Vincent van Hees <vincentvanhees@gmail.com>
2018-01-23 13:22:13 +0100

    code tidied up

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # whitehall-acc
+
+# call_GGIRshell.R
+
+Binary accelerometer data are processed to derive night specific estimates of sleep parameters based on:
+- Heuristic algorithm
+- L5+/-6hr approach
+
+# evaluate_model2.R
+Output from previous step is cleaned and evaluated.
--- evaluate_models2.R
+++ evaluate_models2.R
@@ -3,15 +3,21 @@ graphics.off()
 
 include_auc_calculation = FALSE # takes 5 minutes
 
-# load the data
+# the analyses are done based on the csv output from R package GGIR named part4_....csv.
+# I ran GGIR three times with three configurations and copied the resulting files to the following folders
+# the new algorithm
 path_algorithm = "/media/vincent/Exeter/exeter_2aug/analysis_logFALSE_defnoc1"
-path_logaided = "/media/vincent/Exeter/exeter_2aug/analysis_logTRUE_defnocempty"
+# SPT-window detection as described in the 2015 PLoSONE paper, van Hees, Sabia, et al.
+path_logaided = "/media/vincent/Exeter/exeter_2aug/analysis_logTRUE_defnocempty" #<= not used in paper
+# the L5+6 approach
 path_window = "/media/vincent/Exeter/exeter_2aug/analysis_logFALSE_defnocempty"
+# load the data
 Dalg = read.csv(paste0(path_algorithm,"/part4_nightsummary_sleep_cleaned.csv"))
 Dlog = read.csv(paste0(path_logaided,"/part4_nightsummary_sleep_cleaned.csv"))
 Dwin = read.csv(paste0(path_window,"/part4_nightsummary_sleep_cleaned.csv"))
+# load particpant demographical information
 demogra = read.csv("/media/vincent/Exeter/whitehall_pp_characteristics.csv")
-
+#==============================================================
 # compare and remove non-matching days and individual
 matchdata = function(x,y) {
   fun.12 <- function(x.1,x.2,...){
@@ -43,8 +49,8 @@ Dwin = matcheddata$y
 matcheddata = matchdata(Dlog,Dwin)
 Dlog = matcheddata$x
 Dwin = matcheddata$y
-
-# fix waking time
+# check that waking up times are not wrong by 24 hours
+# and fix waking time if necessary
 fixwakingtime = function(x) {
   x$acc_wake[which(x$acc_wake < x$acc_onset)] = x$acc_wake[which(x$acc_wake < x$acc_onset)] + 24
   x$log_wake[which(x$log_wake < x$log_onset)] = x$log_wake[which(x$log_wake < x$log_onset)] + 24
@@ -53,13 +59,12 @@ fixwakingtime = function(x) {
 Dalg = fixwakingtime(Dalg)
 Dwin = fixwakingtime(Dwin)
 Dlog = fixwakingtime(Dlog)
-# only keep individuals with less than 33% indivalid data
+# only keep nights with less than 33% indivalid data
 removeinvalid =  function(x) x[which(x$fraction_night_invalid < 0.33),]
 Dalg = removeinvalid(Dalg)
 Dlog = removeinvalid(Dlog)
 Dwin = removeinvalid(Dwin)
-
-
+# add a column to specify whether it was a weekend or a weekday
 addweekhalf = function(x) {
   x$weekhalf = "weekenddays"
   x$weekhalf[which(x$weekday %in% c("Sunday","Monday","Tuesday","Wednesday","Thursday") == TRUE)] = "weekdays"
@@ -68,7 +73,7 @@ addweekhalf = function(x) {
 Dalg = addweekhalf(Dalg)
 Dwin = addweekhalf(Dwin)
 Dlog = addweekhalf(Dlog)
-
+# add a column to specify whether it was winter or summer
 addseason = function(x) {
   x$month = sapply(x$calendardate,FUN = function(y) {unlist(strsplit(as.character(y),"/"))[2]})
   x$season = "summer"
@@ -78,10 +83,16 @@ addseason = function(x) {
 Dalg = addseason(Dalg)
 Dwin = addseason(Dwin)
 Dlog = addseason(Dlog)
-
+# merge the three data frames
 Dlogwin = merge(Dlog,Dwin,by=c("id","night"),suffixes = c(".log",".win"))
 D = merge(Dlogwin,Dalg,by=c("id","night"))
 D = D[-which(D$acc_onset == 0 & D$acc_wake ==0),]
+removeimplausable = which(D$acc_dur_noc > D$acc_timeinbed | D$acc_dur_noc.log > D$acc_timeinbed.log | D$acc_dur_noc.win > D$acc_timeinbed.win)
+if (length(removeimplausable) > 0) {
+  D = D[-removeimplausable,]
+  # to do: investigate these 6 nights
+}
+# put relevant variables in a new dataframe
 data = data.frame(id = D$id, night=D$night,
                   durerror_log = D$acc_timeinbed.log - D$sleeplog_timeinbed.log,
                   wakeerror_log = D$acc_wake.log - D$sleeplog_wake.log,
@@ -108,7 +119,7 @@ data = data.frame(id = D$id, night=D$night,
                   wake_win = D$acc_wake.win,
                   wake_alg = D$acc_wake,
                   
-                  sleepeff_log = D$acc_dur_noc.log / D$sleeplog_timeinbed.log,
+                  sleepeff_log = D$acc_dur_noc.log / D$acc_timeinbed.log,
                   sleepeff_win = D$acc_dur_noc.win / D$acc_timeinbed.win,
                   sleepeff_alg = D$acc_dur_noc / D$acc_timeinbed,
                   
@@ -116,32 +127,21 @@ data = data.frame(id = D$id, night=D$night,
                   n_noc_win = D$acc_n_noc.win,
                   n_noc_alg = D$acc_n_noc)
 
-#--------------------------
-# restructure data frame to only include relevant variables
-# data = data.frame(id = Dlog$id, night=Dlog$night,durerror_log = Dlog$acc_timeinbed - Dlog$sleeplog_timeinbed,
-#                   wakeerror_log = Dlog$acc_wake - Dlog$sleeplog_wake,
-#                   onseterror_log = Dlog$acc_onset -  Dlog$sleeplog_onset,
-#                   durerror_win = Dwin$acc_timeinbed - Dlog$sleeplog_timeinbed,
-#                   wakeerror_win = Dwin$acc_wake - Dlog$sleeplog_wake,
-#                   onseterror_win = Dwin$acc_onset -  Dlog$sleeplog_onset,
-#                   durerror_alg = Dalg$acc_timeinbed - Dlog$sleeplog_timeinbed,
-#                   wakeerror_alg = Dalg$acc_wake - Dlog$sleeplog_wake,
-#                   onseterror_alg = Dalg$acc_onset -  Dlog$sleeplog_onset,
-#                   weekhalf = Dlog$weekhalf,season = Dlog$season)
-
-# remove duplicated rows to speed up analysis
+# remove duplicated rows to speed up analysis (not really needed, but good to check anyway)
 data = data[!duplicated(data[,1:6]),]
+# merge data with demographic data to get dataframe for regression model
 modeldata = merge(x=data,y=demogra,by.x="id",by.y="STNO")
 modeldata$SEX = modeldata$SEX - 1 # 0 is men (N = 3552), 1 is women (N=1328)
 modeldata$BMI_uncorrected = modeldata$BMI
+modeldata$age_uncorrected = modeldata$age
 modeldata$age = modeldata$age - mean(modeldata$age,na.rm=TRUE)
 modeldata$BMI = modeldata$BMI - mean(modeldata$BMI,na.rm=TRUE)
 
+# correct 24 hour shifts in log data
 logincorrect = which(modeldata$wake_log < modeldata$onset_log)
 if (length(logincorrect) > 0) {
   modeldata$wake_log[logincorrect] = modeldata$wake_log[logincorrect] + 24
 }
-
 correct_morethan_24 = function(x) {
   lessthan24 = which(x < -24)
   morethan24 = which(x > 24)
@@ -153,7 +153,6 @@ correct_morethan_24 = function(x) {
   }
   return(x)
 }
-
 modeldata$onseterror_alg = correct_morethan_24(modeldata$onseterror_alg)
 modeldata$onseterror_win = correct_morethan_24(modeldata$onseterror_win)
 modeldata$wakeerror_alg = correct_morethan_24(modeldata$wakeerror_alg)
@@ -174,7 +173,6 @@ if (include_auc_calculation == TRUE) {
     rocdata$est[round((x[1]-i0)*Nepochs_hour):i1_est] = 1 # start index for est sleep relative to start : i1_est
     rocdata$dia[round((x[3]-i0)*Nepochs_hour):i1_dia] = 1 # start index for diary sleep relative to start : i1_est
     roccurve = roc(rocdata$dia ~ rocdata$est)
-    print(ci(roccurve))
     return(auc(roccurve))
   }
   rocdata_alg = data.frame(v=modeldata$onset_alg, y=modeldata$wake_alg, z=modeldata$onset_log, w=modeldata$wake_log)
@@ -192,25 +190,37 @@ require(nlme)
 ctrl = lmeControl(opt="optim")
 
 outputmatrix = matrix("",9,9)
-
-jpeg("/media/vincent/Exeter/distributions_whitehall.jpeg",unit="in",res=400,width = 9,height=5)
-par(mfrow=c(1,2))
+jpeg("/media/vincent/Exeter/Figure1.jpeg",unit="in",res=600,width = 7,height=3)
+par(mfrow=c(1,3))
+d1 = density(modeldata$durerror_alg)
+d2 = density(modeldata$durerror_win)
+plot(d1,col="blue",ylim=range(d1$y)*c(1,1.25),xlim=c(-5,5),bty="l",main="Sleep duration",xlab="Difference with diary (hours)",lend=2,
+     lab.cex=0.5,main.cex=0.6,axis.cex=0.5,cex=0.6)
+lines(d2,col="red",lend=2)
+legend("topright",legend = c("Heuristic Algorithm","L5+/-6"),col=c("blue","red"),lty=c(1,1),cex=0.8)
 d1 = density(modeldata$onseterror_alg)
 d2 = density(modeldata$onseterror_win)
-plot(d1,col="blue",xlim=c(-5,5),bty="l",main="Sleep onset",xlab="Difference with diary (hours)",lend=2)
+par(mar=c(5,4,2,1))
+plot(d1,col="blue",ylim=range(d1$y)*c(1,1.25),xlim=c(-5,5),bty="l",main="Sleep onset",xlab="Difference with diary (hours)",lend=2,
+     lab.cex=0.5,main.cex=0.6,axis.cex=0.5,cex=0.5)
 lines(d2,col="red",lend=2)
-legend("topright",legend = c("Algorithm","L5+/-6"),col=c("blue","red"),lty=c(1,1),cex=0.8)
+legend("topright",legend = c("Heuristic Algorithm","L5+/-6"),col=c("blue","red"),lty=c(1,1),cex=0.8)
 d1 = density(modeldata$wakeerror_alg)
 d2 = density(modeldata$wakeerror_win)
-plot(d1,col="blue",xlim=c(-5,5),bty="l",main="Waking up",xlab="Difference with diary (hours)",lend=2)
+plot(d1,col="blue",ylim=range(d1$y)*c(1,1.25),xlim=c(-5,5),bty="l",main="Waking up",xlab="Difference with diary (hours)",lend=2,
+     lab.cex=0.5,main.cex=0.6,axis.cex=0.5,cex=0.6)
 lines(d2,col="red",lend=2)
-legend("topright",legend = c("Algorithm","L5+/-6"),col=c("blue","red"),lty=c(1,1),cex=0.8)
+legend("topright",legend = c("Heuristic Algorithm","L5+/-6"),col=c("blue","red"),lty=c(1,1),cex=0.8)
 dev.off()
 
+# investigate wake duration in relation to error in the estimation of SPT-window duration
+modeldata$awakeduration = modeldata$dur_log * (1-modeldata$sleepeff_log)
+fit.wakeduration_alg = lme(durerror_alg ~ awakeduration, random = ~1|night/id,data=modeldata,control=ctrl,na.action = na.omit)
 
+# aggretate per individual to faciltiate analyses at participant level
 d_expl_BMI_auc = aggregate(modeldata,by = list(modeldata$id),mean)
 
-for (domodel in c("log","alg","win"))  {# "win") {  #c("log","alg","win")) { #
+for (domodel in c("alg", "win"))  {# "win") {  #c("log","alg","win")) { #
   print("-----------------------------------")
   print(domodel)
   ndigits = 1
@@ -254,27 +264,6 @@ for (domodel in c("log","alg","win"))  {# "win") {  #c("log","alg","win")) { #
     sderror_wake = sd(modeldata$wakeerror_log)
     sderror_onset = sd(modeldata$onseterror_log)
   }
-  print(paste0("dur AIC ",AIC(fit.dur)," BIC ",BIC(fit.dur)))
-  print(paste0("onset AIC ",AIC(fit.onset)," BIC ",BIC(fit.onset)))
-  print(paste0("wake AIC ",AIC(fit.wake)," BIC ",BIC(fit.wake)))
-
-  # if (domodel == "alg") {
-  #   fit.dur = lme(durerror_alg ~ SEX, random = ~1|night/id,data=modeldata,control=ctrl,na.action = na.omit)
-  #   fit.wake = lme(wakeerror_alg ~ SEX, random = ~1|night/id,data=modeldata,control=ctrl,na.action = na.omit)
-  #   fit.onset = lme(onseterror_alg ~ SEX, random = ~1|night/id,data=modeldata,control=ctrl,na.action = na.omit)
-  #   matrixpos = 2
-  # } else if (domodel == "win") {
-  #   fit.dur = lme(durerror_win ~ SEX, random = ~1|night/id,data=modeldata,control=ctrl,na.action = na.omit)
-  #   fit.wake = lme(wakeerror_win ~ SEX, random = ~1|night/id,data=modeldata,control=ctrl,na.action = na.omit)
-  #   fit.onset = lme(onseterror_win ~ SEX, random = ~1|night/id,data=modeldata,control=ctrl,na.action = na.omit)
-  #   matrixpos = 3
-  # } else if (domodel == "log") {
-  #   fit.dur = lme(durerror_log ~ SEX, random = ~1|night/id,data=modeldata,control=ctrl,na.action = na.omit)
-  #   fit.wake = lme(wakeerror_log ~ SEX, random = ~1|night/id,data=modeldata,control=ctrl,na.action = na.omit)
-  #   fit.onset = lme(onseterror_log ~ SEX, random = ~1|night/id,data=modeldata,control=ctrl,na.action = na.omit)
-  #   matrixpos = 1
-  # }
-  
   outputmatrix[matrixpos,] = c(getcoef(fit.dur,ndigits),sderror_dur * 60)
   outputmatrix[matrixpos+3,] = c(getcoef(fit.wake,ndigits),sderror_wake * 60)
   outputmatrix[matrixpos+6,] = c(getcoef(fit.onset,ndigits),sderror_onset * 60)
@@ -289,11 +278,11 @@ for (domodel in c("log","alg","win"))  {# "win") {  #c("log","alg","win")) { #
     print(round(tmp,digits=ndigits))
   }
   
-  print(">>>>>>>>>>>>>>> dur")
+  print(">>>>>>>>>>>>>>> SPT window duration")
   printsum2(fit.dur,ndigits)
-  print(">>>>>>>>>>>>>>> wake")
+  print(">>>>>>>>>>>>>>> wake up time")
   printsum2(fit.wake,ndigits)
-  print(">>>>>>>>>>>>>>> onset")
+  print(">>>>>>>>>>>>>>> onset time")
   printsum2(fit.onset,ndigits)
 }
 
